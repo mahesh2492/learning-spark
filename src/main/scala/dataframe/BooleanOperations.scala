e6914123951abe914c262d6b6ad479ada2527f45
Mahesh Chand <mahesh.kndpl@gmail.com>
2019-08-30 18:43:34 +0530

    refactored SparkSession to Cosntants

--- src/main/scala/dataframe/BooleanOperations.scala
+++ src/main/scala/dataframe/BooleanOperations.scala
@@ -1,18 +1,10 @@
 package dataframe
 
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import dataframe.Constants._
 import org.apache.spark.sql.functions._
 
 object BooleanOperations extends App {
 
-  val spark: SparkSession = SparkSession.builder().master("local").appName("dataframe-operations").getOrCreate()
-
-  val df: DataFrame = spark
-    .read
-    .format("csv")
-    .option("header", "true")
-    .option("inferSchema", "true")
-    .load("src/main/resources/retailer.csv")
 
   df.show()
 
