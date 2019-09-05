ed2df55161b1c15645b7d02ea822d70d3580f448
Mahesh Chand <mahesh.kndpl@gmail.com>
2019-09-05 21:45:31 +0530

    added example of spark sql

---
+++ src/main/scala/sql/BasicOperations.scala
@@ -0,0 +1,22 @@
+package sql
+
+import org.apache.spark.sql.SparkSession
+
+object BasicOperations extends App {
+
+
+  val spark = SparkSession.builder().master("local[*]").appName("spark-sql").getOrCreate()
+
+  //reading data as dataFrame
+  val df = spark.read.format("json").load("src/main/resources/2015-summary.json")
+
+  df.show(false)
+  //Df => Sql
+  df.createOrReplaceTempView("dfTable")
+
+  spark.sql("""
+     select DEST_COUNTRY_NAME, sum(count) from dfTable group By DEST_COUNTRY_NAME
+     """.stripMargin)
+    .where("DEST_COUNTRY_NAME like 'S%'").where("`sum(count)` > 10")
+    .show() // SQl => DF
+}
